b424bb0f0d708345af98a4fe30996437b31cfeb1
Hernán D. Mercado <hernandmf@gmail.com>
2022-08-28 00:13:41 -0500

    adding local enviroment variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
+const { config } = require("dotenv");
+
+config();
+
 module.exports = {
-  host: process.env.NEXT_PUBLIC_HOST || "127.0.0.1",
-  database: process.env.NEXT_PUBLIC_DATABASE || "customer-solutions-db",
-  user: process.env.NEXT_PUBLIC_USER || "root",
-  password: process.env.NEXT_PUBLIC_PASSWORD || "password",
+  host: process.env.HOST || "",
+  database: process.env.DATABASE || "",
+  user: process.env.USER || "",
+  password: process.env.PASSWORD || "",
 };
