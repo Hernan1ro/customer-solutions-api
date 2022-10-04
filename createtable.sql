e37f36095dd1b0be706d2f04003abe030cd15fdf
Hernán D. Mercado <hernandmf@gmail.com>
2022-10-03 22:53:46 -0500

    adding questions fields to the database

--- createtable.sql
+++ createtable.sql
@@ -16,3 +16,43 @@ CREATE TABLE `customer-solutions-db`.`test_table` (
     `index` INT NULL,
     PRIMARY KEY (`id`),
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+
+
+--  adding question fields to the user_db
+
+ALTER TABLE `customer-solutions-db`.`user_db` 
+ADD COLUMN `pregunta_0` VARCHAR(250) NOT NULL AFTER `index`,
+ADD COLUMN `pregunta_1` VARCHAR(250) NOT NULL AFTER `pregunta_0`,
+ADD COLUMN `pregunta_2` VARCHAR(250) NOT NULL AFTER `pregunta_1`,
+ADD COLUMN `pregunta_3` VARCHAR(250) NOT NULL AFTER `pregunta_2`,
+ADD COLUMN `pregunta_4` VARCHAR(250) NOT NULL AFTER `pregunta_3`,
+ADD COLUMN `pregunta_5` VARCHAR(250) NOT NULL AFTER `pregunta_4`,
+ADD COLUMN `pregunta_6` VARCHAR(250) NOT NULL AFTER `pregunta_5`,
+ADD COLUMN `pregunta_7` VARCHAR(250) NOT NULL AFTER `pregunta_6`,
+ADD COLUMN `pregunta_8` VARCHAR(250) NOT NULL AFTER `pregunta_7`,
+ADD COLUMN `pregunta_9` VARCHAR(250) NOT NULL AFTER `pregunta_8`,
+ADD COLUMN `pregunta_10` VARCHAR(250) NOT NULL AFTER `pregunta_9`,
+ADD COLUMN `pregunta_11` VARCHAR(250) NOT NULL AFTER `pregunta_10`,
+ADD COLUMN `pregunta_12` VARCHAR(250) NOT NULL AFTER `pregunta_11`,
+ADD COLUMN `pregunta_13` VARCHAR(250) NOT NULL AFTER `pregunta_12`,
+ADD COLUMN `pregunta_14` VARCHAR(250) NOT NULL AFTER `pregunta_13`,
+ADD COLUMN `pregunta_15` VARCHAR(250) NOT NULL AFTER `pregunta_14`,
+ADD COLUMN `pregunta_16` VARCHAR(250) NOT NULL AFTER `pregunta_15`,
+ADD COLUMN `pregunta_17` VARCHAR(250) NOT NULL AFTER `pregunta_16`,
+ADD COLUMN `pregunta_18` VARCHAR(250) NOT NULL AFTER `pregunta_17`,
+ADD COLUMN `pregunta_19` VARCHAR(250) NOT NULL AFTER `pregunta_18`,
+ADD COLUMN `pregunta_20` VARCHAR(250) NOT NULL AFTER `pregunta_19`,
+ADD COLUMN `pregunta_21` VARCHAR(250) NOT NULL AFTER `pregunta_20`,
+ADD COLUMN `pregunta_22` VARCHAR(250) NOT NULL AFTER `pregunta_21`,
+ADD COLUMN `pregunta_23` VARCHAR(250) NOT NULL AFTER `pregunta_22`,
+ADD COLUMN `pregunta_24` VARCHAR(250) NOT NULL AFTER `pregunta_23`,
+ADD COLUMN `pregunta_25` VARCHAR(250) NOT NULL AFTER `pregunta_24`,
+ADD COLUMN `pregunta_26` VARCHAR(250) NOT NULL AFTER `pregunta_25`,
+ADD COLUMN `pregunta_27` VARCHAR(250) NOT NULL AFTER `pregunta_26`,
+ADD COLUMN `pregunta_28` VARCHAR(250) NOT NULL AFTER `pregunta_27`,
+ADD COLUMN `pregunta_29` VARCHAR(250) NOT NULL AFTER `pregunta_28`;
+
+
+-- deleting current test data from the table ---
+
+DELETE FROM user_db 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,6 +12,7 @@ const getUsers = async (req, res) => {
     res.send(err.message);
   }
 };
+
 const addUser = async (req, res) => {
   try {
     const {
@@ -29,6 +30,36 @@ const addUser = async (req, res) => {
       process,
       strategy,
       index,
+      pregunta_0,
+      pregunta_1,
+      pregunta_2,
+      pregunta_3,
+      pregunta_4,
+      pregunta_5,
+      pregunta_6,
+      pregunta_7,
+      pregunta_8,
+      pregunta_9,
+      pregunta_10,
+      pregunta_11,
+      pregunta_12,
+      pregunta_13,
+      pregunta_14,
+      pregunta_15,
+      pregunta_16,
+      pregunta_17,
+      pregunta_18,
+      pregunta_19,
+      pregunta_20,
+      pregunta_21,
+      pregunta_22,
+      pregunta_23,
+      pregunta_24,
+      pregunta_25,
+      pregunta_26,
+      pregunta_27,
+      pregunta_28,
+      pregunta_29,
     } = req.body;
 
     if (
@@ -45,7 +76,37 @@ const addUser = async (req, res) => {
       people === undefined ||
       process === undefined ||
       strategy === undefined ||
-      index === undefined
+      index === undefined ||
+      pregunta_0 === undefined ||
+      pregunta_1 === undefined ||
+      pregunta_2 === undefined ||
+      pregunta_3 === undefined ||
+      pregunta_4 === undefined ||
+      pregunta_5 === undefined ||
+      pregunta_6 === undefined ||
+      pregunta_7 === undefined ||
+      pregunta_8 === undefined ||
+      pregunta_9 === undefined ||
+      pregunta_10 === undefined ||
+      pregunta_11 === undefined ||
+      pregunta_12 === undefined ||
+      pregunta_13 === undefined ||
+      pregunta_14 === undefined ||
+      pregunta_15 === undefined ||
+      pregunta_16 === undefined ||
+      pregunta_17 === undefined ||
+      pregunta_18 === undefined ||
+      pregunta_19 === undefined ||
+      pregunta_20 === undefined ||
+      pregunta_21 === undefined ||
+      pregunta_22 === undefined ||
+      pregunta_23 === undefined ||
+      pregunta_24 === undefined ||
+      pregunta_25 === undefined ||
+      pregunta_26 === undefined ||
+      pregunta_27 === undefined ||
+      pregunta_28 === undefined ||
+      pregunta_29 === undefined
     ) {
       res.status(400).json({ message: "Bad request. Please fill all fields" });
     }
@@ -64,6 +125,36 @@ const addUser = async (req, res) => {
       process,
       strategy,
       index,
+      pregunta_0,
+      pregunta_1,
+      pregunta_2,
+      pregunta_3,
+      pregunta_4,
+      pregunta_5,
+      pregunta_6,
+      pregunta_7,
+      pregunta_8,
+      pregunta_9,
+      pregunta_10,
+      pregunta_11,
+      pregunta_12,
+      pregunta_13,
+      pregunta_14,
+      pregunta_15,
+      pregunta_16,
+      pregunta_17,
+      pregunta_18,
+      pregunta_19,
+      pregunta_20,
+      pregunta_21,
+      pregunta_22,
+      pregunta_23,
+      pregunta_24,
+      pregunta_25,
+      pregunta_26,
+      pregunta_27,
+      pregunta_28,
+      pregunta_29,
     };
 
     const connection = await getConnection();
